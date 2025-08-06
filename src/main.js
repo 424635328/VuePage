03cd90ab1e04a393995ffcb8888fd81addba6241
Tony <424635328@qq.com>
2025-08-06 16:31:19 +0800

    2025-08-06 16:31:12-S.bat

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { MotionPlugin } from '@vueuse/motion'
 
 import App from './App.vue'
 import router from './router'
@@ -14,6 +13,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(MotionPlugin)
 
 app.mount('#app')
